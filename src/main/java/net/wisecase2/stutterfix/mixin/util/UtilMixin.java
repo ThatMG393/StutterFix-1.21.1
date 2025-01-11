95ee2cee8f08321aef2163067d65335582293a8b
ThatMG393 <thatmg393@gmail.com>
2025-01-11 20:08:25 +0800

    Include missing

---
+++ src/main/java/net/wisecase2/stutterfix/mixin/util/UtilMixin.java
@@ -0,0 +1,18 @@
+package net.wisecase2.stutterfix.mixin.util;
+
+import net.minecraft.util.Util;
+import net.wisecase2.stutterfix.StutterFix;
+import org.spongepowered.asm.mixin.*;
+
+import java.util.concurrent.ExecutorService;
+
+@Mixin(Util.class)
+public abstract class UtilMixin {
+	@Overwrite
+	public static ExecutorService getMainWorkerExecutor() {
+		if (!StutterFix.isInitializedMainWorkerExecutor)
+			StutterFix.loadMainWorkerExecutor();
+
+		return StutterFix.mainWorkerExecutor;
+	}
+}
